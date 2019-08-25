5a897b1c2354d712aa1cbef7dc99252ac5a14edc
Cesar Cobo <cdcp88@gmail.com>
2019-08-25 10:25:14 -0500

    -- GalleryFragmentTest
    RecyclerView mounted

---
+++ app/src/androidTest/java/com/cedancp/gallery/GalleryFragmentTest.java
@@ -0,0 +1,52 @@
+package com.cedancp.gallery;
+
+import android.content.Context;
+
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.RootMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.cedancp.gallery.ui.MainActivity;
+import com.cedancp.gallery.ui.view.home.GalleryFragment;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class GalleryFragmentTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<>(
+            MainActivity.class,
+            true,
+            true);
+
+    @Test
+    public void recyclerViewIsMounted() {
+        onView(ViewMatchers.withId(R.id.rv_images))
+                .inRoot(RootMatchers.withDecorView(
+                        Matchers.is(mainActivityRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
+    }
+}
