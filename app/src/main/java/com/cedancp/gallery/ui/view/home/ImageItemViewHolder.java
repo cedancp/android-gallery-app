b615d440d584a8ab644288465f22a3d0c0186e03
Cesar Cobo <cdcp88@gmail.com>
2019-08-26 00:21:14 -0500

    -- code cleaning

--- app/src/main/java/com/cedancp/gallery/ui/view/home/ImageItemViewHolder.java
+++ app/src/main/java/com/cedancp/gallery/ui/view/home/ImageItemViewHolder.java
@@ -1,12 +1,8 @@
 package com.cedancp.gallery.ui.view.home;
 
-import android.view.View;
-
 import androidx.annotation.NonNull;
-import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.cedancp.gallery.R;
 import com.cedancp.gallery.databinding.ImageItemBinding;
 
 public class ImageItemViewHolder extends RecyclerView.ViewHolder {
@@ -17,12 +13,5 @@ public class ImageItemViewHolder extends RecyclerView.ViewHolder {
         super(imageItemBinding.getRoot());
 
         this.imageItemBinding = imageItemBinding;
-
-//        imageItemBinding.getRoot().setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Navigation.findNavController(view).navigate(R.id.imageDetailFragment);
-//            }
-//        });
     }
 }
