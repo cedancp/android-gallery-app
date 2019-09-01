a20262913ae2db3c9319a0008fdaa83e5ba29c7e
Cesar Cobo <cdcp88@gmail.com>
2019-09-01 11:43:07 -0500

    -- Configure with default docker ip

--- app/src/main/java/com/cedancp/gallery/Config.java
+++ app/src/main/java/com/cedancp/gallery/Config.java
@@ -4,7 +4,7 @@ public class Config {
 
     public static final boolean DEBUG = BuildConfig.DEBUG;
 
-    public static final String API_URL = "http:192.168.100.74:3000";
+    public static final String API_URL = "http:172.20.0.5:3000";
 
     public static final int GALLERY_GRID_COLUMNS = 2;
 }
